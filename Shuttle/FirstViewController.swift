02701959ce6a2dda0928cde53422db9f753ad0e5
Nikita Pahadia <Nikita.Pahadia@match.com>
2016-07-20 14:34:46 -0700

    Initial Commit

---
+++ Shuttle/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Shuttle
+//
+//  Created by Nikita Pahadia on 7/20/16.
+//  Copyright © 2016 Nikita Pahadia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Shuttle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Shuttle
+//
+//  Created by Nikita Pahadia on 7/20/16.
+//  Copyright © 2016 Nikita Pahadia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
